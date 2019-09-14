2ea9e9fc5e6a2f59ed2d5d3e8947937b157c2d7b
angadhanoa <54552253+angadhanoa@users.noreply.github.com>
2019-09-13 20:30:31 -0700

    Update proc.c

--- phase1/proc.c
+++ phase1/proc.c
@@ -7,7 +7,6 @@
 #include "proc.h"
 #include "spede.h"
 
-unsigned int sys_time_count = 0; //To count the time for flickering purposes.
 unsigned short *upper_left_pos = (unsigned short *)0xb8000; //declare a pointer and set it to the upper-left display corner
 int flagForDot = 1; //declare and set a flag. When 1 we will show the dot. When 0 we will hide it. 
 
