c6e85b4ea0a42e7211c734caf6e3747a81378401
angadhanoa <54552253+angadhanoa@users.noreply.github.com>
2019-09-15 11:38:25 -0700

    Indentation for comments.

--- phase1/kernel.c
+++ phase1/kernel.c
@@ -4,10 +4,10 @@
 
 #include "spede.h"
 #include "const-type.h"
-#include "entry.h"    // entries to kernel (TimerEntry, etc.)
-#include "tools.h"    // small handy functions
-#include "ksr.h"      // kernel service routines
-#include "proc.h"     // all user process code here
+#include "entry.h"    //entries to kernel (TimerEntry, etc.)
+#include "tools.h"    //small handy functions
+#include "ksr.h"      //kernel service routines
+#include "proc.h"     //all user process code here
 
 /* declare kernel data */
 int run_pid;                  //declare an integer: run_pid;  // current running PID; if -1, none selected
@@ -16,12 +16,12 @@ pcb_t pcb[PROC_MAX];          //declare an array of PCB type: pcb[PROC_MAX];  //
 
 unsigned int sys_time_count;  //declare an unsigned integer: sys_time_count
 struct i386_gate *idt;        //interrupt descriptor table
-int i; 			      //To use with for loops
-char ch; 		      //To store character from keyboard
+int i; 			               //To use with for loops
+char ch; 		               //To store character from keyboard
 
 void BootStrap(void) // set up kernel!
 {            
-   sys_time_count = 0;                          //set sys time count to zero
+   sys_time_count = 0;                    //set sys time count to zero
    Bzero((char *) &avail_que, QUE_MAX);   //call tool Bzero() to clear avail queue
    Bzero((char *) &ready_que, QUE_MAX);   //call tool Bzero() to clear ready queue
    
@@ -40,27 +40,27 @@ void BootStrap(void) // set up kernel!
 int main(void) // OS starts
 {               
    BootStrap();               //do the boot strap things 1st
-   SpawnSR(Idle);             // create Idle thread
+   SpawnSR(Idle);             //create Idle thread
    run_pid = IDLE;            //set run_pid to IDLE
    Loader(pcb[run_pid].tf_p); //call Loader() to load the trapframe of Idle
-   return 0;                  // never would actually reach here
+   return 0;                  //never would actually reach here
 }
 
 void Scheduler(void) // choose a run_pid to run
 {              
    if(run_pid > IDLE) 
    {   
-      return;       // a user PID is already picked
+      return;                       //a user PID is already picked
    }
    if(QueEmpty(&ready_que)) 
    {
-      run_pid = IDLE;               // use the Idle thread
+      run_pid = IDLE;               //use the Idle thread
    } else 
    {
       pcb[IDLE].state = READY;
-      run_pid = DeQue(&ready_que);  // pick a different proc
+      run_pid = DeQue(&ready_que);  //pick a different proc
    }
-   pcb[run_pid].time_count = 0;     // reset runtime count
+   pcb[run_pid].time_count = 0;     //reset runtime count
    pcb[run_pid].state = RUN;
 }
 
