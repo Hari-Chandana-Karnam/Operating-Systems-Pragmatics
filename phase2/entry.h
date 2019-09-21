96864e7f19ca1585e1b1881caf1fd02c89b57280
angadhanoa <54552253+angadhanoa@users.noreply.github.com>
2019-09-20 17:17:33 -0700

    Added SyscallEntry() prototype.

--- phase2/entry.h
+++ phase2/entry.h
@@ -8,10 +8,11 @@
                    // since below is not in assembler syntax
 __BEGIN_DECLS
 
-#include "const-type.h"    // tf_t below needs it
+#include "const-type.h"   // tf_t below needs it
 
-void TimerEntry(void);     // coded in entry.S, assembler won't like this syntax
-void Loader(tf_t *);       // coded in entry.S
+void TimerEntry(void);    // coded in entry.S, assembler won't like this syntax
+void Loader(tf_t *);      // coded in entry.S
+void SyscallEntry(void);  // coded in entry.S
 
 __END_DECLS
 
