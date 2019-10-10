984a26527672edb472aa76178ba26d08fc19dfc9
Angad Dhanoa <54552253+angadhanoa@users.noreply.github.com>
2019-10-09 20:37:58 -0700

    Updated for phase 5. Completed.

--- phase5/syscall.h
+++ phase5/syscall.h
@@ -9,8 +9,10 @@ void sys_sleep(int sleep_sec);
 void sys_write(char* write_sec);
 void sys_set_cursor(int row, int column); 
 int sys_fork(void); 
-unsigned int sys_get_rand(void);
-void sys_lock_mutex(int mutex_id);
-void sys_unlock_mutex(int mutex_id); 
+unsigned int sys_get_rand(void);          // phase 4
+void sys_lock_mutex(int mutex_id);        // phase 4
+void sys_unlock_mutex(int mutex_id);      // phase 4
+void sys_exit(int exit_code);             // phase 5
+int sys_wait(int *exit_code);             // phase 5
 
 #endif
