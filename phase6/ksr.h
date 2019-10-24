3aa68ddaa538f37b1bdd1d7b44f634d58af24556
Angad Dhanoa <54552253+angadhanoa@users.noreply.github.com>
2019-10-23 20:58:35 -0700

    From Phase 5 - Working

---
+++ phase6/ksr.h
@@ -0,0 +1,20 @@
+// ksr.h, 159
+
+#ifndef _KSR_
+#define _KSR_
+
+#include "const-type.h"
+
+void SpawnSR(func_p_t p);
+void TimerSR(void);
+void SyscallSR(void);
+void SysSleep(void);
+void SysWrite(void);
+void SysSetCursor(void);
+void SysFork(void);
+void SysLockMutex(void);
+void SysUnlockMutex(void);
+void SysExit(void);
+void SysWait(void);
+
+#endif
