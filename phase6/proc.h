99835557a4b902664e92531ee1721e1bf320c0bf
Angad Dhanoa <54552253+angadhanoa@users.noreply.github.com>
2019-10-29 19:55:30 -0700

    Working Phase 6

---
+++ phase6/proc.h
@@ -0,0 +1,10 @@
+// proc.h, 159
+
+#ifndef _PROC_
+#define _PROC_
+
+void Idle(void); //coded in proc.c
+void Init(void);
+void MyChildExitHandler(void);
+
+#endif
---
+++ phase6/tools.h
@@ -0,0 +1,18 @@
+// tools.h, 159
+
+#ifndef _TOOLS_
+#define _TOOLS_
+
+#include "const-type.h"
+
+//Prototypes for all the functions in tools.c
+int QueEmpty(que_t *ptr);
+int QueFull(que_t *ptr);
+int DeQue(que_t *ptr);
+void EnQue(int data, que_t *ptr);
+void Bzero(char *start, unsigned int max);
+void MemCpy(char *dst, char *src, unsigned int max);
+void Number2Str(int x, char *str);
+
+#endif
+
