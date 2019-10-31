c65d5dcd3b542640e0a79df996c9552ce4f9c63c
Angad Dhanoa <54552253+angadhanoa@users.noreply.github.com>
2019-10-30 19:33:04 -0700

    Updated for Phase 7.

--- phase7/syscall.h
+++ phase7/syscall.h
@@ -14,7 +14,8 @@ void      sys_lock_mutex(int mutex_id);             // phase 4
 void      sys_unlock_mutex(int mutex_id);           // phase 4
 void      sys_exit(int exit_code);                  // phase 5
 int       sys_wait(int *exit_code);                 // phase 5
-void      sys_signal(int signal_name, func_p_t p);   // phase 6
-void      sys_kill(int pid, int signal_name);        // phase 6
+void      sys_signal(int signal_name, func_p_t p);  // phase 6
+void      sys_kill(int pid, int signal_name);       // phase 6
+void      sys_read(char *str);                      // phase 7
 
 #endif
