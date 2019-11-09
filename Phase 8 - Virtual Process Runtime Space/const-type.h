5654c51faacc7dcc95200f27978354cbad9cac10
Angad Dhanoa <54552253+angadhanoa@users.noreply.github.com>
2019-11-08 18:03:08 -0800

    Added constants, element in PCB, and new page_t structure.

--- Phase 8 - Virtual Process Runtime Space/const-type.h
+++ Phase 8 - Virtual Process Runtime Space/const-type.h
@@ -9,16 +9,20 @@
 #define PIC_CONT_REG 0x20        // I/O loc # of PIc control
 #define TIMER_SERVED_VAL 0x60    // control code sent to PIC
 #define VGA_MASK_VAL 0x0f00      // bold face, white on black
+#define PRESENT 0x01             // Page present flag
+#define RW 0x02                  // Page is both read & writable
+#define RO 0x00                  // Page is read only
 
 #define TIME_MAX 310             // max timer count, then rotate process
 #define PROC_MAX 20              // max number of processes
 #define STACK_MAX 4096           // process stack in bytes
 #define QUE_MAX 20               // capacity of a process queue
 #define STR_MAX 20
+#define PAGE_MAX 100             // OS has 100 DRAM pages to dispatch
+#define PAGE_SIZE 4096           // Each page size in bytes
 
 #define NONE -1                  // to indicate none
 #define IDLE 0                   // Idle thread PID 0
-#define DRAM_START 0xe00000      // 14 MB
 #define VIDEO_MUTEX 0            // ID of VIDEO_MUTEX is 0. Phase 4.
 #define UNLOCKED 0               // mutex unlocked state is 0
 #define LOCKED 1                 // mutex locked state is 1
@@ -39,6 +43,7 @@
 #define SYS_SIGNAL 140           // phase 6
 #define SYS_KILL 141             // phase 6
 #define SYS_READ 142             // phase 7
+#define SYS_VFORK 143            // phase 8 - creation of a virtual-space running process
 
 // Signals
 #define SIGCHLD 17               // phase 6
@@ -46,6 +51,9 @@
 
 #define VIDEO_START (unsigned short *)0xb8000
 #define VIDEO_END ((unsigned short *)0xb8000 + 25 * 80)
+#define DRAM_START 0xe00000         // 14 MB
+#define G1 0x40000000               // phase 8 - Virtual space starts
+#define G2 0x80000000               // phase 8 - Vitual space ends (1 less byte)
 
 typedef void (*func_p_t)(void);
 
@@ -56,13 +64,14 @@ typedef struct {
 } tf_t;  // 'trapframe' type
 
 typedef struct {
-   state_t state;                // State of the system.
-   tf_t *tf_p;                   // Trapframe.
-   unsigned int time_count;      // time_count is the timer for the processes 
-   unsigned int total_time;      // total_time is the CPU Run Time.
-   unsigned int wake_time;       // wake_time is when the CPU will wake up.
-   unsigned int ppid;            // Process ID of parent.
-   func_p_t signal_handler[32];  // 32 is the number of total signals possible in a 32-bit OS.
+    state_t state;                  // State of the system.
+    tf_t *tf_p;                     // Trapframe.
+    unsigned int time_count;        // time_count is the timer for the processes 
+    unsigned int total_time;        // total_time is the CPU Run Time.
+    unsigned int wake_time;         // wake_time is when the CPU will wake up.
+    unsigned int ppid;              // Process ID of parent.
+    func_p_t signal_handler[32];    // 32 is the number of total signals possible in a 32-bit OS.
+    unsigned dir;                   // This is where the address-translation directory is.
 } pcb_t;    //PCB type
 
 typedef struct {
@@ -79,5 +88,14 @@ typedef struct {
    que_t buffer;
    que_t wait_que;
 } kb_t;
+
+typedef struct { 
+    int pid  the process that uses the page (initially NONE)
+    union {                  // u is an alias of 3 meanings:
+        unsigned addr;        // its real byte address
+        char *content;        // as ptr to page content (bytes)
+        unsigned *entry;      // as an array, used as ...u.entry[..]
+    } u;
+} page_t;
 
 #endif      // to prevent name mangling
