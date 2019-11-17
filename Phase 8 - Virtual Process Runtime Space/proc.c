39303a0a2cf7e9d17ce235da66dedd0db2a41b56
HARI CHANDANA KARNAM RAJENDRAPRASAD <47847700+Hari-Chandana-Karnam@users.noreply.github.com>
2019-11-16 16:47:23 -0800

    updated shellcal

--- Phase 8 - Virtual Process Runtime Space/proc.c
+++ Phase 8 - Virtual Process Runtime Space/proc.c
@@ -160,3 +160,16 @@ void Login(void)
 		}
 	}
 }
+
+void ShellCal(){
+	sys_write("    November 2019\r");
+	sys_write("Su Mo Tu We Th Fr Sa\r");
+	sys_write(" 1  2\r");
+	sys_write(" 3  4  5  6  7  8  9\r");
+	sys_write("10 11 12 13 14 15 16\r");
+	sys_write("17 18 19 20 21 22 23\r");
+	sys_write("24 25 26 27 28 29 30\r");
+}
+
+
+
