850f2d9dee191573c471e74586de32bc05faaabe
HARI CHANDANA KARNAM RAJENDRAPRASAD <47847700+Hari-Chandana-Karnam@users.noreply.github.com>
2019-11-17 19:03:22 -0800

    Update ksr.c

--- Phase 8 - Virtual Process Runtime Space/ksr.c
+++ Phase 8 - Virtual Process Runtime Space/ksr.c
@@ -221,7 +221,7 @@ void SysFork(void)
 	pcb[PID].tf_p->ebx = 0;
 	
 	pcb[PID].Dir = KDir;  //set Dir in PCB to KDir for the new process (so it'll use real memory),
-	/*mark down the equivalent DRAM page to be occupied by the new process
+	page[PID].pid = PID;/*mark down the equivalent DRAM page to be occupied by the new process
    (e.g., Idle and Login), so the page array can skip these already used*/
 }
 
