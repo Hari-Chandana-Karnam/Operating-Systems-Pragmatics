c18b7c98808077ce9b7e68544d5acc32f0b3abde
dcace <32889707+dcace@users.noreply.github.com>
2019-12-02 21:17:54 -0800

    fixed some of the grading issues (not sure if all)
    the terminal is now much faster

---
+++ phase9/kernel.c
@@ -0,0 +1,149 @@
+// kernel.c, 158, phase 4
+//
+// Team Name: "RuntimeErrors" (Members: Angad Pal Dhanoa, Dalton Caraway, & Hari Chandana Karnam)
+
+#include "spede.h"
+#include "const-type.h"
+#include "entry.h"    //entries to kernel (TimerEntry, etc.)
+#include "tools.h"    //small handy functions
+#include "ksr.h"      //kernel service routines
+#include "proc.h"     //all user process code here
+#include "syscall.h"
+
+/* declare kernel data */
+int run_pid;                  //declare an integer: run_pid;  // current running PID; if -1, none selected
+que_t avail_que, ready_que;   //declare 2 queues: avail_que and ready_que;  // avail PID and those created/ready to run
+pcb_t pcb[PROC_MAX];          //declare an array of PCB type: pcb[PROC_MAX];  // Process Control Blocks
+mutex_t video_mutex;
+kb_t kb;
+tty_t tty;  		//use tty_t to declare a 'tty' to interface a terminal port
+page_t  page[PAGE_MAX];
+unsigned int KDir;			  // Kernals's 'real address' - translation directory;
+unsigned int sys_time_count;  //declare an unsigned integer: sys_time_count
+unsigned short *sys_cursor;   //Add the new cursor position that OS keep
+unsigned sys_rand_count;		
+struct i386_gate *idt;        //interrupt descriptor table
+
+void BootStrap(void) 
+{
+   	int i; 
+   	sys_time_count = 0;                    		//set sys time count to zero  	
+	sys_cursor = VIDEO_START;               	//have it set to VIDEO_START in BootStrap()
+	sys_rand_count = 0;							//set sys rand count to zero 
+	video_mutex.lock = UNLOCKED;				//set the lock of video_mutex to be UNLOCKED
+   	KDir = get_cr3();
+	
+	Bzero((char *) &avail_que, sizeof(que_t));  				//call tool Bzero() to clear avail queue
+   	Bzero((char *) &ready_que, sizeof(que_t));  				//call tool Bzero() to clear ready queue
+	Bzero((char *) &video_mutex.suspend_que, sizeof(que_t));	//call tool Bzero() to clear video_mutex's suspend queue
+	Bzero((char *) &kb, sizeof(kb_t));
+	
+   //enqueue all the available PID numbers to avail queue
+   for(i = 0; i < QUE_MAX; i++)
+   {
+      EnQue(i, &avail_que);
+   }
+	
+   /* For the page array:
+    * each page is used by NONE, and its
+    * page[i].u.addr = DRAM_START + i * PAGE_SIZE where i = 0..PAGE_MAX-1
+	*/
+   for(i = 0; i <= PAGE_SIZE -1; i++)
+   {
+	   page[i].pid = NONE;
+	   page[i].u.addr = DRAM_START + i * PAGE_SIZE;
+   }
+
+    idt = get_idt_base();                                                      	  //get IDT location
+    fill_gate(&idt[TIMER_EVENT], (int)TimerEntry, get_cs(), ACC_INTR_GATE, 0);	  //addr of TimerEntry is placed into proper IDT entry
+    fill_gate(&idt[SYSCALL_EVENT], (int)SyscallEntry, get_cs(), ACC_INTR_GATE, 0); //Have to program this one properly.
+	fill_gate(&idt[TTY_EVENT], (int)TTYEntry, get_cs(), ACC_INTR_GATE, 0);
+	outportb(PIC_MASK_REG, PIC_MASK_VAL); //send PIC control register the mask value for timer handling
+}
+
+
+void TTYinit(void) {                // phase9
+      int i, j;
+      
+      Bzero((char *)&tty, sizeof(tty_t));
+      tty.port = TTY0;
+
+      outportb(tty.port+CFCR, CFCR_DLAB);             // CFCR_DLAB is 0x80
+      outportb(tty.port+BAUDLO, LOBYTE(115200/9600)); // period of each of 9600 bauds
+      outportb(tty.port+BAUDHI, HIBYTE(115200/9600));
+      outportb(tty.port+CFCR, CFCR_PEVEN|CFCR_PENAB|CFCR_7BITS);
+   
+      outportb(tty.port+IER, 0);
+      outportb(tty.port+MCR, MCR_DTR|MCR_RTS|MCR_IENABLE);
+
+      for(i=0; i<166667; i++)asm("inb $0x80");       // wait .1 sec
+      outportb(tty.port+IER, IER_ERXRDY|IER_ETXRDY); // enable TX & RX intr
+      for(i=0; i<166667; i++)asm("inb $0x80");       // wait .1 sec
+
+      for(j=0; j<3; j++) {                           // clear 3 lines
+         outportb(tty.port, 'V');
+         for(i=0; i<83333; i++)asm("inb $0x80");     // wait .5 sec should do
+         outportb(tty.port, '\n');
+         for(i=0; i<83333; i++)asm("inb $0x80");
+         outportb(tty.port, '\r');
+         for(i=0; i<83333; i++)asm("inb $0x80");
+      }
+      inportb(tty.port);                             // get 1st key PROCOMM logo
+      for(i=0; i<83333; i++)asm("inb $0x80");        // wait .5 sec
+}
+
+
+int main(void) 
+{
+   BootStrap();               	//do the boot strap things 1st
+   TTYinit();		        //to initialize a terminal port	
+   SpawnSR(&Idle);             	//create Idle thread
+   SpawnSR(&Login);             	//create Init thread
+   run_pid = IDLE;           	//set run_pid to IDLE
+   set_cr3(pcb[run_pid].dir);
+   Loader(pcb[run_pid].tf_p); 	//call Loader() to load the trapframe of Idle
+   return 0; 					//never would actually reach here
+}
+
+void Scheduler(void) // choose a run_pid to run
+{              
+   if(run_pid > IDLE)    
+      return;                       //a user PID is already picked
+   
+   if(QueEmpty(&ready_que)) 
+      run_pid = IDLE;               //use the Idle thread
+   else 
+   {
+      pcb[IDLE].state = READY;
+      run_pid = DeQue(&ready_que);  //pick a different proc
+   }
+   pcb[run_pid].time_count = 0;     //reset runtime count
+   pcb[run_pid].state = RUN;
+}
+
+void Kernel(tf_t *tf_p) // kernel runs
+{      
+    pcb[run_pid].tf_p = tf_p; //copy tf_p to the trapframe ptr (in PCB) of the process in run
+
+    switch(tf_p->event) 
+    {
+        case TIMER_EVENT:
+            TimerSR();         // handle tiemr event
+      	    break;
+   		case SYSCALL_EVENT:
+      	    SyscallSR();       // all syscalls go here 1st
+      	    break;
+		case TTY_EVENT:      // phase9
+			 TTYSR();
+			 break;
+		default:
+      	    cons_printf("Kernel Panic: no such event!\n");
+			breakpoint();
+	}
+	
+	KBSR();
+   	Scheduler();               //call Scheduler() to change run_pid if needed
+   	set_cr3(pcb[run_pid].dir);
+	Loader(pcb[run_pid].tf_p); //call Loader() to load the trapframe of the selected process
+}
+
---
+++ phase9/proc.c
@@ -0,0 +1,341 @@
+// proc.c, 159
+// processes are coded here
+
+#include "const-type.h"  // VGA_MASK_VAL
+#include "ext-data.h"    // sys_time_count
+#include "syscall.h"     // sys service calls
+#include "tools.h"       // Number2Str()
+#include "spede.h"
+#include "proc.h"
+
+unsigned short *upper_left_pos = (unsigned short *)0xb8000;
+int flagForDot = 1; 
+
+void Idle(void)
+{
+    int i;
+    while(1)
+    {     
+		sys_rand_count++;        
+		if((sys_time_count % 100) == 0) //Using mod to detmine the multiple of 100.
+        {
+            if (flagForDot == 1)
+            {
+                *upper_left_pos = '*' + VGA_MASK_VAL; //VGA_MASK_VAL makes it bold and white on black.
+                for(i = 0; i < 50; i++) //Added this loop to delay the rocess for half a second. This may stop te flickering
+                {
+                    asm("sti");
+                }
+                flagForDot = 0;
+            }
+			else if (flagForDot == 0)
+            {
+                *upper_left_pos = ' ' + VGA_MASK_VAL; //VGA_MASK_VAL makes it bold and white on black.
+                for(i = 0; i < 50; i++) //Added this loop to delay the rocess for half a second. This may stop te flickering
+                {
+                	asm("sti");
+                }
+                flagForDot = 1;
+            }
+        }
+    }
+}
+
+void MyChildExitHandler(void)
+{
+	int exit_code, exit_cpid;
+	char pid_str[PROC_MAX], str[PROC_MAX];
+	
+	exit_cpid = sys_wait(&exit_code);				
+	
+    Number2Str(exit_cpid, pid_str); 	
+    Number2Str(exit_code, str); 	
+
+    sys_lock_mutex(VIDEO_MUTEX);		
+    sys_set_cursor(exit_cpid, 72); 		
+    sys_write(pid_str);					
+    sys_write(":");						
+    sys_write(str);						
+    sys_unlock_mutex(VIDEO_MUTEX);
+}
+
+void Init(void) {
+   	int my_pid, column, forked_pid, i, sleep_period; 
+	int total_sleep_period;
+   	char pid_str[PROC_MAX];
+	
+   sys_signal(SIGCHLD, MyChildExitHandler);
+
+	for(i = 0; i < 5; i++) 
+	{
+		forked_pid = sys_fork();
+		if(forked_pid == 0)
+		{
+			break;
+		}
+   		else if(forked_pid == NONE)
+		{
+			sys_write("sys_fork() failed!\n");
+			sys_exit(NONE);
+		}
+	}
+	
+	my_pid = sys_get_pid(); 
+	Number2Str(my_pid, pid_str);
+
+	if(forked_pid > 0) 
+	{	
+		sys_sleep(10);			
+		sys_kill(0, SIGCONT);
+
+		while(1)
+		{
+        	sys_lock_mutex(VIDEO_MUTEX);
+            sys_set_cursor(my_pid, 0);
+            sys_write(pid_str);
+            sys_unlock_mutex(VIDEO_MUTEX);
+            
+			sys_sleep(10);
+            
+			sys_lock_mutex(VIDEO_MUTEX);
+            sys_set_cursor(my_pid, 0);
+            sys_write("-");
+			if(my_pid > 9)
+				sys_write("-");
+            sys_unlock_mutex(VIDEO_MUTEX);
+       		
+			sys_sleep(10);
+   		}
+    }
+	
+	sys_sleep(1000000);
+	column = 0;
+	total_sleep_period = 0;
+	while(column < 70)
+	{			
+		sys_lock_mutex(VIDEO_MUTEX);
+		sys_set_cursor(my_pid, column);
+		sys_write(pid_str);
+		sys_unlock_mutex(VIDEO_MUTEX);	
+		
+		sleep_period =  1 + ((sys_get_rand()/my_pid) % 4);
+		sys_sleep(sleep_period);
+		total_sleep_period += sleep_period;
+		
+		sys_lock_mutex(VIDEO_MUTEX);
+		sys_set_cursor(my_pid, column);
+		sys_write(".");
+		if(my_pid > 9)
+		{
+			sys_set_cursor(my_pid, column+1);
+			sys_write(" ");
+		}
+		sys_unlock_mutex(VIDEO_MUTEX);
+		
+		column++;
+	}
+	sys_exit(total_sleep_period); 	
+}
+
+
+
+void ShellDir(void)
+{
+	int row, column;
+	if(sys_cursor >= VIDEO_START + 16*80 )
+	{
+		row = 0;
+		sys_cursor = VIDEO_START;
+		while(row < 25)
+		{
+			column = 0;
+			while(column < 80)
+			{
+				*sys_cursor = ' ' + VGA_MASK_VAL;
+				sys_cursor++;
+				column++;
+			}
+			row++;
+		}
+		sys_cursor = VIDEO_START;
+		sys_write(" Wrapped cursor last command> dir\r\n");
+	}	
+	sys_write("     Desktop\r\n");   
+	sys_write("     Documents \r\n");   
+	sys_write("     Downloads \r\n");
+	sys_write("     Music\r\n");  
+	sys_write("     Pictures\r\n"); 
+	sys_write("     Public\r\n");  
+	sys_write("     Templates\r\n");   
+	sys_write("     Videos\r\n");   
+	sys_exit(0);
+}
+
+void ShellCal(void)
+{	
+	int row, column;
+	
+	if(sys_cursor >= VIDEO_START + 17*80 )
+	{
+		row = 0;
+		sys_cursor = VIDEO_START;
+		while(row < 25)
+		{
+			column = 0;
+			while(column < 80)
+			{
+				*sys_cursor = ' ' + VGA_MASK_VAL;
+				sys_cursor++;
+				column++;
+			}
+			row++;
+		}
+		sys_cursor = VIDEO_START;
+		sys_write(" Wrapped cursor last command> cal\r\n");
+	}
+	
+	sys_write("        November 2019     \r\n");
+	sys_write("     Su Mo Tu We Th Fr Sa \r\n");
+	sys_write("                     1  2 \r\n");
+	sys_write("      3  4  5  6  7  8  9 \r\n");
+	sys_write("     10 11 12 13 14 15 16 \r\n");
+	sys_write("     17 18 19 20 21 22 23 \r\n");
+	sys_write("     24 25 26 27 28 29 30 \r\n");
+	sys_exit(0);
+}
+
+void ShellRoll(void)
+{
+	int row, column, dice1, dice2;
+	
+	if(sys_cursor >= VIDEO_START + 23*80 )
+	{
+		row = 0;
+		sys_cursor = VIDEO_START;
+		while(row < 25)
+		{
+			column = 0;
+			while(column < 80)
+			{
+				*sys_cursor = ' ' + VGA_MASK_VAL;
+				sys_cursor++;
+				column++;
+			}
+			row++;
+		}
+		sys_cursor = VIDEO_START;
+		sys_write(" Wrapped cursor last command> roll\r\n");
+	}	
+
+
+	dice1 = (sys_get_rand() % 6) + 1;
+	dice2 = (sys_get_rand() % 6) + 1;
+	sys_exit(dice1 + dice2);
+}
+
+void Shell (void)
+{  	
+	int exit_code, exit_cpid;
+	char pid_str[PROC_MAX], str[PROC_MAX];
+	char input_str[STR_MAX];
+	char dir_str[] = "dir";
+	char cal_str[] = "cal";
+	char roll_str[] = "roll";
+    while(1)
+	{	
+		sys_write("Runtime Errors> ");        
+		sys_read(input_str);
+
+		if(StringCompare(dir_str, input_str) == 1)
+		{		
+			sys_vfork(&ShellDir);	
+			exit_cpid = sys_wait(&exit_code);
+			Number2Str(exit_cpid, pid_str); 	
+		    Number2Str(exit_code, str);
+			sys_write(" Exit PID: ");									
+			sys_write(pid_str);
+			sys_write("     Exit Code: ");
+			sys_write(str);
+			sys_write("\r\n");
+		}
+		else if(StringCompare(cal_str, input_str) == 1)
+		{		
+			sys_vfork(&ShellCal);	
+			exit_cpid = sys_wait(&exit_code);
+			Number2Str(exit_cpid, pid_str); 	
+		    Number2Str(exit_code, str);
+			sys_write(" Exit PID: ");									
+			sys_write(pid_str);
+			sys_write("     Exit Code: ");
+			sys_write(str);
+			sys_write("\r\n");
+		}
+		else if(StringCompare(roll_str, input_str) == 1)
+		{		
+			sys_vfork(&ShellRoll);	
+			exit_cpid = sys_wait(&exit_code);
+			Number2Str(exit_cpid, pid_str); 	
+		    Number2Str(exit_code, str);
+			sys_write(" Exit PID: ");									
+			sys_write(pid_str);
+			sys_write("     Exit Code: ");
+			sys_write(str);
+			sys_write("\r\n");
+		}
+		else
+		{
+			int row, column;
+			if(sys_cursor >= VIDEO_START + 20*80 )
+			{
+				row = 0;
+				sys_cursor = VIDEO_START;
+				while(row < 25)
+				{
+					column = 0;
+					while(column < 80)
+					{
+						*sys_cursor = ' ' + VGA_MASK_VAL;
+						sys_cursor++;
+						column++;
+					}
+					row++;
+				}
+				sys_cursor = VIDEO_START;
+				sys_write(" Wrapped cursor last command was invalid\r\n");
+			}				
+			sys_write("     Valid commands are:\r\n");
+			sys_write("          dir  -- display directory cotent.\r\n");
+			sys_write("          cal  -- display calender.\r\n");
+        	sys_write("          roll -- roll a pair of dice.\r\n"); 
+		}				
+	}
+}
+
+void Login(void) 
+{
+    char login_str[STR_MAX], password_str[STR_MAX];
+    
+	while(1) 
+	{
+    	sys_write("Login: ");         
+	    sys_read(login_str);
+		sys_write("\r\n");
+		sys_write("Password: ");
+		sys_read(password_str);
+		sys_write("\r\n");
+		
+        if(StringCompare(login_str, password_str) == 1)
+		{
+			sys_write("Login Successful!\r\n");
+			sys_vfork(&Shell);
+			sys_exit(0);
+		}
+		else
+		{
+			sys_write("Login Failed!\r\n");
+		}
+	}
+}
+
+
+
